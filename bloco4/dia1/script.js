83c49d00154715f8b90a15041228417aac715ffa
AdelinoJnr <adelinojunior96@gmail.com>
2021-02-24 15:54:28 -0300

    finalizando parte 3 das praticas da aula

--- bloco4/dia1/script.js
+++ bloco4/dia1/script.js
@@ -1,16 +1,41 @@
-const name = "Adelino Junior"
-const birthCity = "Divinópolis"
-let birthYear = 1996
+// Parte 2
 
-console.log(name)
-console.log(birthCity)
-console.log(birthYear)
+// const name = "Adelino Junior";
+// const birthCity = "Divinópolis";
+// let birthYear = 1996;
 
-birthYear = 2020
+// console.log(name);
+// console.log(birthCity);
+// console.log(birthYear);
 
-console.log(birthYear)
+// birthYear = 2020;
 
-birthCity = "Nova York"
+// console.log(birthYear);
 
-// Acontece um erro por tentar alterar uma constante.
-console.log(birthCity)
+// birthCity = "Nova York";
+
+// console.log(birthCity);
+
+// Parte 3
+
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientId);
+// console.log(typeof isEnrolled);
+// console.log(typeof patientInfo);
+// console.log(typeof patientEmail);
+
+// patientId = '50'
+
+// console.log(typeof patientId);
+
+// let base = 5;
+// let altura = 8;
+// let area = base * altura;
+// let perimetro = (base ** 2) + (altura ** 2);
