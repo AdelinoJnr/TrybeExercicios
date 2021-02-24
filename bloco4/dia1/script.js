a4b0449f391cadaab22e432e8da492b96d8acb42
AdelinoJnr <adelinojunior96@gmail.com>
2021-02-24 15:16:46 -0300

    COmpletando o exercicio 4.1 parte2

---
+++ bloco4/dia1/script.js
@@ -0,0 +1,16 @@
+const name = "Adelino Junior"
+const birthCity = "Divinópolis"
+let birthYear = 1996
+
+console.log(name)
+console.log(birthCity)
+console.log(birthYear)
+
+birthYear = 2020
+
+console.log(birthYear)
+
+birthCity = "Nova York"
+
+// Acontece um erro por tentar alterar uma constante.
+console.log(birthCity)
