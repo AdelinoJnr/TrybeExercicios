6af274a76bb5b867f87158d57fc43c8d7676f10a
AdelinoJnr <adelinojunior96@gmail.com>
2021-02-24 18:25:04 -0300

    completando os exercicios de 1 a 5

---
+++ bloco4/dia1/excerses-dia1.js
@@ -0,0 +1,68 @@
+// Exercicio 1
+
+// let a = 10;
+// let b = 50;
+// let soma;
+// let subtracao;
+// let multiplicacao;
+// let divisao;
+
+// //Adição
+// soma = a + b;
+
+// //Subtração
+// subtracao = a - b;
+
+// //Multiplicação
+// multiplicacao = a * b;
+
+// //Divisão
+// divisao = a / b;
+
+// Exercicio 2
+
+// if (a > b) {
+//     console.log('o numero maior é: ' + a)
+// } else if (b > a) {
+//     console.log('o numero maior é: ' + b)
+// } else {
+//     console.log('os numeros são iguais')
+// }
+
+
+// Exercicio 3
+
+// let a = 5;
+// let b = 10;
+// let c = 15;
+
+// if (a > b || a > c) {
+//     console.log('O numero maior é: ' + a)
+// } else if (b > a || b > c) {
+//     console.log('O numero maior é: ' + b)
+// } else {
+//     console.log('O numero maior é: ' + c)
+// }
+
+
+// Exercicio 4
+
+// let a = -6;
+
+// if (a > 0) {
+//     console.log('O numero ' + a + ' é positivo')
+// } else if (a == 0) {
+//     console.log('O numero é igual a zero')
+// } else if (a < 0) {
+//     console.log('O numero ' + a + ' é negativo')
+// }
+
+// Exercicio 5
+
+// let ang1 = 60;
+// let ang2 = 70;
+// let ang3 = 50;
+
+// let soma = (ang1 + ang2 + ang3) == 180;
+
+// console.log(soma)
