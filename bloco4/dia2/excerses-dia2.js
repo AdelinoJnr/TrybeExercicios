b5636254047bb78adc5bd679fb0296af92de1397
AdelinoJnr <adelinojunior96@gmail.com>
2021-02-25 18:36:38 -0300

    Resoluçao do exercicio 2

--- bloco4/dia2/excerses-dia2.js
+++ bloco4/dia2/excerses-dia2.js
@@ -2,6 +2,16 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // Exercicio 1
 
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index])
+// }
+
+// Exercico 2
+
+let soma = 0;
+
 for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index])
+    soma += numbers[index]
 }
+
+console.log(soma)
