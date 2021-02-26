49dcf68f9d604f040c1a426b34f611cbd8c5c2e5
AdelinoJnr <adelinojunior96@gmail.com>
2021-02-26 18:43:39 -0300

    Resoluçao do exercicio 1

---
+++ bloco4/dia3/exercicio-dia3.js
@@ -0,0 +1,21 @@
+// Exercicio 1
+
+// let n = 5;
+// let ast = '*';
+
+
+// if (n > 1) {
+
+//     for (let index1 = 1; index1 < n; index1 += 1) {
+
+//         ast += '*';
+//     }
+
+//     for (let index2 = 0; index2 < n; index2 += 1) {
+//         console.log(ast)
+//     }
+
+// } else {
+//     console.log('Número invalido')
+// }
+
