3d042ec5135af5805bde9c64b6db0bbe5ba89b79
AdelinoJnr <adelinojunior96@gmail.com>
2021-02-26 18:54:44 -0300

    Resolvendo os exercicio 2 do dia 3

--- bloco4/dia3/exercicio-dia3.js
+++ bloco4/dia3/exercicio-dia3.js
@@ -19,3 +19,16 @@
 //     console.log('Número invalido')
 // }
 
+// Exercicio 2
+
+// let n = 5;
+// let ast = '';
+
+// for (let index1 = 0; index1 < n; index1 += 1) {
+  
+//   ast += '*';
+//   console.log(ast)
+// }
+
+// Exercicio 3
+
