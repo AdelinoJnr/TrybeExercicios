8ca58f1f75e0e3fd40c89cd78f2459b471ef395c
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-01 19:12:35 -0300

    Resoluçao do exercicio 2

--- bloco4/dia4/exercises-dia4.js
+++ bloco4/dia4/exercises-dia4.js
@@ -1,11 +1,11 @@
 // Parte 1
 
 
-let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-};
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+// };
 
 // console.log('Bem-vinda, ' + info.personagem);
 
@@ -21,11 +21,38 @@ let info = {
 //   console.log(key);
 // }
 
-let info = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O ultimo MacPatinhas',
-  recorrente: 'Sim'
-}
+// let info = {
+//   personagem: 'Tio Patinhas',
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: 'O ultimo MacPatinhas',
+//   recorrente: 'Sim'
+// }
 
-console.log(info.personagem)
+// Parte 2
+
+// Exercicio 1
+
+// function returnPalindromo(string) {
+
+// }
+
+// Exercicio 2
+
+// function maiorNumeroInteiro(numerosInteiros) {
+
+//   let indexMaiorValor = 0;
+  
+//   for (let index in numerosInteiros) {
+    
+//     if (numerosInteiros[index] > numerosInteiros[indexMaiorValor]) {
+//       indexMaiorValor = index;
+//     }
+  
+//   }
+
+//   return indexMaiorValor;
+// }
+
+// let numerosInteiros = [2, 3, 6, 7, 10, 1];
+
+// console.log(maiorNumeroInteiro(numerosInteiros));
