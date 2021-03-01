841c1f898070cb0352cc4e02fa9a9843a175e7e7
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-01 18:33:06 -0300

    Resolvendo exercicios do dia 4 parte 1

---
+++ bloco4/dia4/exercises-dia4.js
@@ -0,0 +1,31 @@
+// Parte 1
+
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+
+// console.log('Bem-vinda, ' + info.personagem);
+
+// info['recorrente'] = 'Sim';
+
+// console.log(info);
+
+// for (let key in info) {
+//   console.log(info[key]);
+// }
+
+// for (let key in info) {
+//   console.log(key);
+// }
+
+let info = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O ultimo MacPatinhas',
+  recorrente: 'Sim'
+}
+
+console.log(info.personagem)
