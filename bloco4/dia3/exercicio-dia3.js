b6fd3c2737c7e96fa7cf32319f4526c4a3954eba
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-01 10:40:49 -0300

    Resoluçao do exercicio 3

--- bloco4/dia3/exercicio-dia3.js
+++ bloco4/dia3/exercicio-dia3.js
@@ -32,3 +32,25 @@
 
 // Exercicio 3
 
+// let n = 5;
+// let simbol = '*';
+// let inputLine = '';
+// let delimitador = n;
+
+// for (let index = 0; index < n; index += 1) {
+
+//   for (let index2 = 1; index2 <= n; index2 += 1) {
+
+//     if (index2 < delimitador) {
+//       inputLine += ' '
+//     } else {
+//       inputLine += simbol
+//     }
+//   }
+//   console.log(inputLine);
+//   inputLine = '';
+//   delimitador -= 1;
+// }
+
+// Exercicio 4
+
