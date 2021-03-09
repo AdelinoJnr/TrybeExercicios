2370f8107eb0de30aa87a71bacb4856ab72cbfb6
AdelinoJnr <adelinojunior96@gmail.com>
2021-03-09 11:08:15 -0300

    Resoluçao doexercicio 5

--- bloco5/dia3/script.js
+++ bloco5/dia3/script.js
@@ -16,8 +16,10 @@ createDaysOfTheWeek();
 // Escreva seu código abaixo.
 
 // Exercicio 1
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+const selectDiv = document.querySelector('.buttons-container');
+
 function createDaysCalender() {
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
   const myUlList = document.getElementById('days')
 
   for (let index = 0; index < dezDaysList.length; index += 1) {
@@ -37,11 +39,51 @@ function createDaysCalender() {
 createDaysCalender();
 
 // Exercicio 2
-function createBtnFeriados(btnString) {
+function createBtnFeriados(btnStringHoliday) {
   const btnFeriados = document.createElement('button');
-  const selectDiv = document.querySelector('.buttons-container')
-  btnFeriados.innerHTML = btnString;
+  btnFeriados.className = 'btn-feriados'
+  btnFeriados.innerHTML = btnStringHoliday;
   selectDiv.appendChild(btnFeriados);
 }
 
 createBtnFeriados('Feriados');
+
+// Exercicio 3
+
+function colorBtnHoliday() {
+  const listHoliday = document.getElementsByClassName('holiday');
+  const btnFeriados = document.querySelector('.btn-feriados');
+  btnFeriados.addEventListener('click', function() {
+    for (let index = 0; index < listHoliday.length; index += 1) {
+      listHoliday[index].style.backgroundColor = "rgb(238,12,238)";
+    }
+  })
+}
+colorBtnHoliday()
+
+// Exercicio 4
+
+function createBtnFriday(btnStringFriday) {
+  let btn = document.createElement('button');
+  btn.id = 'btn-friday';
+  btn.innerHTML = btnStringFriday;
+  selectDiv.appendChild(btn);
+}
+
+createBtnFriday('Sexta-Feira')
+
+function colorBtnFriday() {
+  let divFridays = document.getElementsByClassName('friday');
+  let btn = document.getElementById('btn-friday');
+  let innerTextSextou = 'SEXTOOU'
+  btn.addEventListener('click', function() {
+    for (let index = 0; index < divFridays.length; index += 1) {
+      if (divFridays[index].innerHTML !== innerTextSextou) {
+        divFridays[index].innerHTML = innerTextSextou;
+      } else {
+        divFridays[index].innerHTML = divFridays[index].innerHTML;
+      }
+    }
+  });
+}
+colorBtnFriday();
