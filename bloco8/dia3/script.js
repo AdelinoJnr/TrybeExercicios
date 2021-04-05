9905452d44a4837870fed352e649c82d8f9cf2cf
AdelinoJnr <adelinojunior96@gmail.com>
2021-04-05 18:07:23 -0300

    Resoluçao dos exercicios 4 e 6 do dia

--- bloco8/dia3/script.js
+++ bloco8/dia3/script.js
@@ -83,103 +83,103 @@
 
 // Parte 2
 
-const assert = require('assert');
+// const assert = require('assert');
 
-const books = [
-  {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
-  {
-    id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
-    author: {
-      name: 'Stephen King',
-      birthYear: 1947,
-    },
-    releaseYear: 1986,
-  },
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: {
-      name: 'H. P. Lovecraft',
-      birthYear: 1890,
-    },
-    releaseYear: 1928,
-  },
-];
+// const books = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'George R. R. Martin',
+//       birthYear: 1948,
+//     },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'J. R. R. Tolkien',
+//       birthYear: 1892,
+//     },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Isaac Asimov',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Frank Herbert',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: {
+//       name: 'Stephen King',
+//       birthYear: 1947,
+//     },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: {
+//       name: 'H. P. Lovecraft',
+//       birthYear: 1890,
+//     },
+//     releaseYear: 1928,
+//   },
+// ];
 
-const expectedResult = [
-  {
-    age: 31,
-    author: 'Isaac Asimov',
-  },
-  {
-    age: 38,
-    author: 'H. P. Lovecraft',
-  },
-  {
-    age: 39,
-    author: 'Stephen King',
-  },
-  {
-    age: 43,
-    author: 'George R. R. Martin',
-  },
-  {
-    age: 45,
-    author: 'Frank Herbert',
-  },
-  {
-    age: 62,
-    author: 'J. R. R. Tolkien',
-  },
-];
+// const expectedResult = [
+//   {
+//     age: 31,
+//     author: 'Isaac Asimov',
+//   },
+//   {
+//     age: 38,
+//     author: 'H. P. Lovecraft',
+//   },
+//   {
+//     age: 39,
+//     author: 'Stephen King',
+//   },
+//   {
+//     age: 43,
+//     author: 'George R. R. Martin',
+//   },
+//   {
+//     age: 45,
+//     author: 'Frank Herbert',
+//   },
+//   {
+//     age: 62,
+//     author: 'J. R. R. Tolkien',
+//   },
+// ];
 
-function nameAndAge() {
-  return books.map((livro) =>({ age: (livro.releaseYear - livro.author.birthYear),author: livro.author.name, })).sort((num1, num2) => num1.age - num2.age); 
-}
+// function nameAndAge() {
+//   return books.map((livro) =>({ age: (livro.releaseYear - livro.author.birthYear),author: livro.author.name, })).sort((num1, num2) => num1.age - num2.age); 
+// }
 
-assert.deepStrictEqual(nameAndAge(), expectedResult);
+// assert.deepStrictEqual(nameAndAge(), expectedResult);
 
 // Parte 3
 
@@ -300,100 +300,103 @@ assert.deepStrictEqual(nameAndAge(), expectedResult);
 
 // Parte 4
 
-const assert = require('assert');
+// const assert = require('assert');
 
-const books = [
-  {
-    id: 1,
-    name: 'As Crônicas de Gelo e Fogo',
-    genre: 'Fantasia',
-    author: {
-      name: 'George R. R. Martin',
-      birthYear: 1948,
-    },
-    releaseYear: 1991,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: {
-      name: 'J. R. R. Tolkien',
-      birthYear: 1892,
-    },
-    releaseYear: 1954,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Isaac Asimov',
-      birthYear: 1920,
-    },
-    releaseYear: 1951,
-  },
-  {
-    id: 4,
-    name: 'Duna',
-    genre: 'Ficção Científica',
-    author: {
-      name: 'Frank Herbert',
-      birthYear: 1920,
-    },
-    releaseYear: 1965,
-  },
-  {
-    id: 5,
-    name: 'A Coisa',
-    genre: 'Terror',
-    author: {
-      name: 'Stephen King',
-      birthYear: 1947,
-    },
-    releaseYear: 1986,
-  },
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: {
-      name: 'H. P. Lovecraft',
-      birthYear: 1890,
-    },
-    releaseYear: 1928,
-  },
-];
+// const books = [
+//   {
+//     id: 1,
+//     name: 'As Crônicas de Gelo e Fogo',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'George R. R. Martin',
+//       birthYear: 1948,
+//     },
+//     releaseYear: 1991,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: {
+//       name: 'J. R. R. Tolkien',
+//       birthYear: 1892,
+//     },
+//     releaseYear: 1954,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Isaac Asimov',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 4,
+//     name: 'Duna',
+//     genre: 'Ficção Científica',
+//     author: {
+//       name: 'Frank Herbert',
+//       birthYear: 1920,
+//     },
+//     releaseYear: 1965,
+//   },
+//   {
+//     id: 5,
+//     name: 'A Coisa',
+//     genre: 'Terror',
+//     author: {
+//       name: 'Stephen King',
+//       birthYear: 1947,
+//     },
+//     releaseYear: 1986,
+//   },
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: {
+//       name: 'H. P. Lovecraft',
+//       birthYear: 1890,
+//     },
+//     releaseYear: 1928,
+//   },
+// ];
 
-const expectedResult = [
-  {
-    id: 6,
-    name: 'O Chamado de Cthulhu',
-    genre: 'Terror',
-    author: { name: 'H. P. Lovecraft', birthYear: 1890 },
-    releaseYear: 1928,
-  },
-  {
-    id: 3,
-    name: 'Fundação',
-    genre: 'Ficção Científica',
-    author: { name: 'Isaac Asimov', birthYear: 1920 },
-    releaseYear: 1951,
-  },
-  {
-    id: 2,
-    name: 'O Senhor dos Anéis',
-    genre: 'Fantasia',
-    author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
-    releaseYear: 1954,
-  },
-];
+// const expectedResult = [
+//   {
+//     id: 6,
+//     name: 'O Chamado de Cthulhu',
+//     genre: 'Terror',
+//     author: { name: 'H. P. Lovecraft', birthYear: 1890 },
+//     releaseYear: 1928,
+//   },
+//   {
+//     id: 3,
+//     name: 'Fundação',
+//     genre: 'Ficção Científica',
+//     author: { name: 'Isaac Asimov', birthYear: 1920 },
+//     releaseYear: 1951,
+//   },
+//   {
+//     id: 2,
+//     name: 'O Senhor dos Anéis',
+//     genre: 'Fantasia',
+//     author: { name: 'J. R. R. Tolkien', birthYear: 1892 },
+//     releaseYear: 1954,
+//   },
+// ];
 
-function oldBooks() {
-  // escreva seu código aqui
-}
+// function oldBooks() {
+//   const livros60anos = books.filter((item) => {
+//     return (2021 - item.releaseYear) > 60
+//   }).sort((num1, num2) => num1.releaseYear - num2.releaseYear);
+//   return livros60anos;
+// }
 
-assert.deepStrictEqual(oldBooks(), expectedResult);
+// assert.deepStrictEqual(oldBooks(), expectedResult);
 
 // Parte 5 
 
@@ -550,7 +553,8 @@ const expectedResult = [
 ];
 
 function oldBooks() {
-  // escreva seu código aqui
+  const livros60anos = books.filter((livro) => (2021 - livro.releaseYear) > 60).map((livro) => `${livro.name}`);
+  return livros60anos;
 }
 
 assert.deepStrictEqual(oldBooks(), expectedResult);
