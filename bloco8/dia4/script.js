b8e9ef5e9e8fe614cc56f5c568d6adca79cbf614
AdelinoJnr <adelinojunior96@gmail.com>
2021-04-07 15:07:32 -0300

    Resoluçao do exercicio do dia 4

--- bloco8/dia4/script.js
+++ bloco8/dia4/script.js
@@ -3,13 +3,13 @@
 const assert = require('assert');
 
 const arrays = [
-    ['1', '2', '3'],
-    [true],
-    [4, 5, 6],
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
 ];
 
 function flatten() {
-  // escreva seu código aqui
+  return arrays.reduce((acc, item) => acc.concat(item), []);
 }
 
 assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
@@ -82,9 +82,229 @@ const books = [
   },
 ];
 
+const lastElement = (acc, item, index, array) => {
+  if(index === array.length - 1) return `${acc} ${item.author.name}.`;
+  return `${acc} ${item.author.name},`;
+};
+
 function allNames() {
-  // escreva seu código aqui
+  return books.reduce(lastElement, 'Nomes:')
 }
 
 assert.deepStrictEqual(allNames(), 'Nomes: George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.');
 
+// Parte 3
+
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expectedResult = 43;
+
+const mediaDeIdade = (acc, item) => {
+  const total = acc + (item.releaseYear - item.author.birthYear);
+  return total;
+};
+
+function averageAge() {
+  return books.reduce(mediaDeIdade, 0) / books.length;
+}
+
+assert.strictEqual(averageAge(), expectedResult);
+// Parte 4 
+
+const assert = require('assert');
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function longestNamedBook() {
+  return books.reduce((acc, item) => {
+    if (item.name.length > acc.name.length) {
+      return item;
+    }
+    return acc;
+  });
+}
+
+assert.deepStrictEqual(longestNamedBook(), expectedResult);
+
+// Parte 5 
+
+
+const assert = require('assert');
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA() {
+  let cont = 0;
+  names.reduce((acc, item) => {
+    item.toLowerCase().split('').forEach((letra) => {
+      if(letra.includes('a')) return cont += 1;
+    });
+  }, 0);
+  return cont;
+}
+
+assert.deepStrictEqual(containsA(), 20);
+
+// Parte 6
+
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  const objeto = students.map((item, index) => {
+    return {
+      name: item,
+      average: grades[index].reduce((acc, nota) => (acc + nota), 0) / grades[index].length,
+    } 
+  });
+  return objeto;
+}
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+console.log(studentAverage());
+assert.deepStrictEqual(studentAverage(), expected);
