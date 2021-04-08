253d55905518719381dc03313fcb3708b657372b
AdelinoJnr <adelinojunior96@gmail.com>
2021-04-07 22:56:14 -0300

    Resolução da parte 5 a 8

--- bloco8/dia5/script.js
+++ bloco8/dia5/script.js
@@ -95,3 +95,86 @@ const filteredPeople = filterPeople(people);
 
 assert.deepStrictEqual(filteredPeople[0], { name: 'Nicole', bornIn: 1992, nationality: 'Australian' });
 assert.deepStrictEqual(filteredPeople[1], { name: 'Toby', bornIn: 1901, nationality: 'Australian' });
+
+// Parte 5
+
+const assert = require('assert');
+
+const myList = [1, 2, 3];
+
+// escreva swap abaixo
+const swap = (myList) => {
+  return [myList[2], myList[1], myList[0]];
+};
+
+const swappedList = swap(myList);
+
+assert.strictEqual(swappedList[0], 3);
+assert.strictEqual(swappedList[1], 2);
+assert.strictEqual(swappedList[2], 1);
+
+// Parte 6
+
+const assert = require('assert');
+
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+// escreva toObject abaixo
+const toObject = (arr) => {
+  return {
+    name: arr[0],
+    brand: arr[1],
+    year: arr[2],
+  }
+};
+
+
+assert.deepStrictEqual(toObject(palio), { name: 'Palio', brand: 'Fiat', year: 2019 });
+assert.deepStrictEqual(toObject(shelbyCobra), { name: 'Shelby Cobra', brand: 'Ford', year: 1963 });
+assert.deepStrictEqual(toObject(chiron), { name: 'Chiron', brand: 'Bugatti', year: 2016 });
+
+// Parte 7
+
+const assert = require('assert');
+
+const ships = [
+  {
+      name: 'Titanic',
+      length: 269.1,
+      measurementUnit: 'meters',
+  },
+  {
+      name: 'Queen Mary 2',
+      length: 1132,
+      measurementUnit: 'feet',
+  },
+  {
+      name: 'Yamato',
+      length: 256,
+      measurementUnit: 'meters',
+  },
+];
+
+// escreva shipLength abaixo
+const shipLength = ({name, length, measurementUnit}) => {
+  return `${name} is ${length} ${measurementUnit} long`;
+};
+
+assert.strictEqual(shipLength(ships[0]), 'Titanic is 269.1 meters long');
+assert.strictEqual(shipLength(ships[1]), 'Queen Mary 2 is 1132 feet long');
+assert.strictEqual(shipLength(ships[2]), 'Yamato is 256 meters long');
+
+// Parte 8
+
+const assert = require('assert');
+
+// escreva greet abaixo
+const greet = (name, saudacao = 'Hi') => {
+  return `${saudacao} ${name}`
+};
+
+assert.strictEqual(greet('John'), 'Hi John');
+assert.strictEqual(greet('John', 'Good morning'), 'Good morning John');
+assert.strictEqual(greet('Isabela', 'Oi'), 'Oi Isabela');
